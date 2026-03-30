1cbae5c5376613231fec5411227a475483cac18c
Saurav Tomar <tomarsaurav10@gmail.com>
2026-03-30 22:18:23 +0530

    updated project (fixed API & socket issues)

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api/v1": "/",
+    baseURL: import.meta.env.MODE === "development"
+        ? "http://localhost:4000/api/v1"
+        : import.meta.env.VITE_API_URL,
     withCredentials: true,
-})
+});
