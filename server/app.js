4915ddaf17f0336b725bfc4512b2b5ae23300897
Saurav Tomar <tomarsaurav10@gmail.com>
2026-03-30 22:28:20 +0530

    fixed cors origin

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ config({ path: "./config/config.env" });
         cors({
             origin: [
                 "http://localhost:5173",
-                "https://real-time-chat-application-one-iota.vercel.app/login"
+                "https://real-time-chat-application-one-iota.vercel.app"
             ],
             credentials: true,
             methods: ["GET", "POST", "PUT", "DELETE"],
